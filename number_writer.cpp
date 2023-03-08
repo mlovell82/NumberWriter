724d4727efb7d934414be0bc76af1987a6ab9d00
Mick Lovell <mlovell@kent.edu>
2023-03-08 13:01:48 -0500

    Create number_writer.cpp

---
+++ number_writer.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+    int number;
+    std::cout<< "Enter a number!" << std::endl;
+    std::cin>> number;
+    std::cout<< "You've entered " << number << std::endl;
+return 0;
+}
